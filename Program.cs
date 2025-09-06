e5734badf33f0f265e940c40b356aa7308657a90
MohammadJavad <mojavadssh@gmail.com>
2025-09-06 11:14:14 +0330

    Apply .gitignore

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using api.Data;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();  // for controllers
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers(); // swagger will not work without it
+
+app.Run();
