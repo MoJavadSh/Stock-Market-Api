6b74d65a9ea34c2ed210c16a8ad218b27fdad4a2
MohammadJavad <mojavadssh@gmail.com>
2025-09-07 15:13:38 +0330

    feat: add Delete endpoint for Stock in controller

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -19,6 +19,7 @@ namespace api.Controllers
         {
             _context = context;
         }
+        
         [HttpGet]
         public IActionResult GetAll()
         {
@@ -69,6 +70,17 @@ namespace api.Controllers
             _context.SaveChanges();
             
             return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+                return NotFound();
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
         }
 
 
